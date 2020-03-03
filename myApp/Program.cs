a27cdce51cfbedbd4fbe3b40a3337721c679156f
nazirovlix <nazirovlix@gmail.com>
2020-03-03 10:11:33 +0500

    init

---
+++ myApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Me", 1000);
+            Summ(account);
+
+            Console.WriteLine(account.Balance);
+            Console.WriteLine(account.GetAccountHistory());
+        }
+
+        public static void Summ(BankAccount account)
+        {
+            List<Transaction> noteList = new List<Transaction>();
+            noteList.Add(new Transaction(350, DateTime.Now, "Olma"));
+            noteList.Add(new Transaction(200, DateTime.Now, "Olcha"));
+            noteList.Add(new Transaction(220, DateTime.Now, "Behi"));
+
+            foreach (Transaction item in noteList)
+            {
+                account.MakeWithdrawal(item.Amount, item.Date, item.Notes);
+            }
+        }
+
+    }
+}
